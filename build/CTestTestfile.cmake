78ef97c5ef321e6645244aca95bfd0cccbb3dc96
David Flores <a01209414@itesm.mx>
2022-04-26 18:33:27 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dfd/ProyRob/src
+# Build directory: /home/dfd/ProyRob/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dfd/ProyRob/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dfd/puzzlebot_ws/devel;/home/dfd/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/dfd/puzzlebot_ws/devel;/home/dfd/catkin_ws/devel;/opt/ros/melodic' != '' else []

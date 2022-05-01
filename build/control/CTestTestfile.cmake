b835fec0b8a4cb4c507ca6975fe91401d36a4381
David Flores <a01209414@itesm.mx>
2022-05-01 13:34:30 -0500

    Corrected Repo

--- build/control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dfd/ProyRob/src/control
-# Build directory: /home/dfd/ProyRob/build/control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/roboclaw_ros/roboclaw_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dfd/ProyRob/src/roboclaw_ros/roboclaw_node
-# Build directory: /home/dfd/ProyRob/build/roboclaw_ros/roboclaw_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

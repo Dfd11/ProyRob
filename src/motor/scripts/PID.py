415c9416e233786023f297c55e54537387706b8b
dfd <a01209414@itesm.mx>
2022-05-24 02:33:14 +0000

    Finished files for SLAM

--- src/motor/scripts/PID.py
+++ src/motor/scripts/PID.py
@@ -3,11 +3,11 @@ import rospy
 from geometry_msgs.msg import Twist
 const_L=0.422148
 const_R=0.061595
-kp=1
-ki=0.0
-kd=0.0
+#kp=1
+#ki=0.0
+#kd=0.0
 Freq=10
-elapsed_time = 1.0/Freq
+#elapsed_time = 1.0/Freq
 MAX_ANG_VEL = 15
 class PIDClass():
     def __init__(self):
@@ -15,43 +15,45 @@ class PIDClass():
 
         self.pub_wvel = rospy.Publisher('speed_rad', Twist, queue_size=1)
         rospy.Subscriber("speed", Twist, self.update_setpoint)
-        rospy.Subscriber("odom_rad", Twist, self.update_current)
+#        rospy.Subscriber("odom_rad", Twist, self.update_current)
         #********** INIT NODE **********###
-        self.prev_error_l = 0.0
-        self.prev_error_r = 0.0
+#        self.prev_error_l = 0.0
+#        self.prev_error_r = 0.0
         self.setpoint_wl = 0.0
         self.setpoint_wr = 0.0
-        self.current_wl = 0.0
-        self.current_wr = 0.0
-        self.error_l = 0.0
-        self.error_r = 0.0
-        self.cum_error_l = 0.0
-        self.cum_error_r = 0.0
-        self.rate_error_l = 0.0
-        self.rate_error_r = 0.0
-        self.out_l = 0.0
-        self.out_r = 0.0
+#        self.current_wl = 0.0
+#        self.current_wr = 0.0
+#        self.error_l = 0.0
+#        self.error_r = 0.0
+#        self.cum_error_l = 0.0
+#        self.cum_error_r = 0.0
+#        self.rate_error_l = 0.0
+#        self.rate_error_r = 0.0
+#        self.out_l = 0.0
+#        self.out_r = 0.0
         self.out = Twist()
         r = rospy.Rate(Freq)
         print("Node initialized 10.0hz")
         while not rospy.is_shutdown():
-            self.prev_error_l=self.error_l
-            self.prev_error_r=self.error_r
+#            self.prev_error_l=self.error_l
+#            self.prev_error_r=self.error_r
 
-            self.error_l = self.setpoint_wl - self.current_wl
-            self.error_r = self.setpoint_wr - self.current_wr
+#            self.error_l = self.setpoint_wl - self.current_wl
+#            self.error_r = self.setpoint_wr - self.current_wr
 
-            self.cum_error_l = self.cum_error_l + self.error_l * elapsed_time
-            self.cum_error_r = self.cum_error_r + self.error_r * elapsed_time
+#            self.cum_error_l = self.cum_error_l + self.error_l * elapsed_time
+#            self.cum_error_r = self.cum_error_r + self.error_r * elapsed_time
 
-            self.rate_error_l = (self.error_l - self.prev_error_l) / elapsed_time
-            self.rate_error_r = (self.error_r - self.prev_error_r) / elapsed_time
+#            self.rate_error_l = (self.error_l - self.prev_error_l) / elapsed_time
+#            self.rate_error_r = (self.error_r - self.prev_error_r) / elapsed_time
 
-            self.out_l = kp * self.error_l + ki * self.cum_error_l + kd * self.rate_error_l
-            self.out_r = kp * self.error_r + ki * self.cum_error_r + kd * self.rate_error_r
+#            self.out_l = kp * self.error_l + ki * self.cum_error_l + kd * self.rate_error_l
+#            self.out_r = kp * self.error_r + ki * self.cum_error_r + kd * self.rate_error_r
 
-            self.out.angular.x=self.out_l
-            self.out.angular.y=self.out_r
+            #self.out.angular.x=self.out_l
+            #self.out.angular.y=self.out_r
+            self.out.angular.x=self.setpoint_wl
+            self.out.angular.y=self.setpoint_wr
             print("hello")
             self.pub_wvel.publish(self.out)
 
@@ -71,9 +73,9 @@ class PIDClass():
 
         if (self.setpoint_wr < -MAX_ANG_VEL):
             self.setpoint_wr = -MAX_ANG_VEL
-    def update_current(self, msg):
-        self.current_wl = msg.angular.x
-        self.current_wr = msg.angular.y
+#    def update_current(self, msg):
+#        self.current_wl = msg.angular.x
+#        self.current_wr = msg.angular.y
 
     def cleanup(self):
         #This function is called just before finishing the node
